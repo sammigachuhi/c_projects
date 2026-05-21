e548a868f2330027294d8011fd07d4d9a0d827f0
sammigachuhi <sammigachuhi@gmail.com>
2026-05-21 10:03:06 +0300

    easy cmath

---
+++ Section5/MathFun/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    int powResult = pow(2, 3);
+    int sqrtResult = sqrt(25);
+    int ceilResult = ceil(4.2);
+    int floorResult = floor(4.2);
+    int NUM = 512;
+    int log2Result = log2(NUM);
+
+    cout << "2^3 is " << powResult << endl;
+    cout << "sqrt of 25 is " << sqrtResult << endl;
+    cout << "ceiling of 4.2 is " << ceilResult << endl;
+    cout << "floor of 4.2 is " << floorResult << endl;
+    cout << "The log2 of " << NUM << " is " << log2Result << endl;
+    
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
