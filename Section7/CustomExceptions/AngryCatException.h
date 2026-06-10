1ed2fbca90dabe681766753d11d4f2969cb5539e
sammigachuhi <sammigachuhi@gmail.com>
2026-06-10 09:50:26 +0300

    custom exceptions

---
+++ Section7/CustomExceptions/AngryCatException.h
@@ -0,0 +1,24 @@
+#ifndef ANGRY_CAT_EXCEPTION_H 
+#define ANGRY_CAT_EXCEPTION_H 
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+using namespace std;
+
+class AngryCatException : public runtime_error {
+    public: 
+        AngryCatException() : runtime_error("Made kitty angry!") {
+
+        }
+
+        AngryCatException(const string& err) : runtime_error(err) {
+            
+        }
+        
+};
+
+
+
+
+#endif
